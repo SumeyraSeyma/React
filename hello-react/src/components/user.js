4b90191d6fc3da0c38432834c6d290229843b7f5
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-08-13 08:47:29 +0300

    proptypes

--- hello-react/src/components/user.js
+++ hello-react/src/components/user.js
@@ -1,9 +1,11 @@
+import PropTypes from 'prop-types';
+
 function User ({isLoggedIn, name, surname, age, friends}) {
     return (
     
     <>
     <h1>
-    {isLoggedIn ? `${name} ${surname} (${age})` : 'Giriş Yapınız'}
+    {isLoggedIn ? `${name} ${surname} (${age})` : 'Giriş Yapmadınız'}
     </h1>;
 
     {
@@ -15,5 +17,13 @@ function User ({isLoggedIn, name, surname, age, friends}) {
     );
     
 }
+
+User.propTypes = {
+    name: PropTypes.string.isRequired,
+    surname: PropTypes.string.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    age: PropTypes.number,
+    friends: PropTypes.array,
+};
 
 export default User;
